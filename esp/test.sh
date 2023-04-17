8f69725351372f53657a8853807c81a06c81cd89
Giulio Girardi <giulio.girardi@protechgroup.it>
2023-04-18 00:00:43 +0200

    Add esp testing

--- esp/test.sh
+++ esp/test.sh
@@ -5,4 +5,18 @@ set -e
 
 . assert.sh
 
+# Test toolchain components
+assert_raises "xtensa-esp32-elf-gcc --version"
+assert_raises "xtensa-esp32-elf-gdb --version"
+assert_raises "xtensa-esp32-elf-objcopy --version"
+assert_raises "make --version"
+assert_raises "ninja --version"
+assert_raises "cmake --version"
+
+# Test tools
+assert_raises "idf.py --version"
+
+# Test programming tools
+assert_raises "openocd --version"
+
 assert_end esp
