b6263dbcbe46f4337b67fd106bb6ccfecb588d70
Giulio Girardi <giulio.girardi@protechgroup.it>
2023-12-01 08:39:41 +0100

    Add tests

--- barearmgui/test.sh
+++ barearmgui/test.sh
@@ -5,4 +5,12 @@ set -e
 
 . assert.sh
 
+# Load emscripten
+. /opt/emsdk-3.1.5/emsdk_env.sh
+
+# Build tools
+assert_raises "gcc --version"
+assert_raises "x86_64-w64-mingw32-gcc --version"
+assert_raises "emcc --version"
+
 assert_end barearmgui
